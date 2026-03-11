d5a41df349d42997dadd2b54a0cebc1aeabe46e1
Elias Ravichagua <egrp21405@gmail.com>
2026-03-11 05:59:16 -0500

    Actualizacion v1

--- src/main/java/com/engineai/crfe/CrfeServiceApplication.java
+++ src/main/java/com/engineai/crfe/CrfeServiceApplication.java
@@ -3,10 +3,15 @@ package com.engineai.crfe;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.TimeZone;
+
 @SpringBootApplication
 public class CrfeServiceApplication {
 
     public static void main(String[] args) {
+
+        TimeZone.setDefault(TimeZone.getTimeZone("America/Lima"));
+
         SpringApplication.run(CrfeServiceApplication.class, args);
     }
 }
